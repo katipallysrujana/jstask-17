ec6f074d5157ef41dd528dadaa6cf7a8251c46e1
unknown <katipallysrujana@gmail.com>
2024-11-09 01:04:56 +0530

    strings

---
+++ task.js
@@ -0,0 +1,34 @@
+// let str = "Hello World!";
+// Relace the substring “World!” with “My Dear Friend” ..
+// Write the program for that
+let str="Hello World!";
+console.log(str.replace("My Dear Friend"))
+
+
+
+// let a=”Hungry”;
+// Repeat the “Hungry” for 10 times
+// Write the program for that
+let a="Hungry";
+console.log(a.repeat(10))
+
+
+
+// let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+// Find the last index of the string “fox”
+let senetence="The quick brown fox jumps over the lazy dog. The fox is clever."
+console.log(senetence.lastIndexOf("fox"));
+
+// let message = "Hello, world!";
+// Find the string starts with “hi”
+// Write code for that
+let message = "Hello, world!";
+let b=message.startsWith("hi")
+console.log(b)
+
+
+// let fileName = "document.pdf";
+// Find the string ends with “.pdf”
+// Write code for that
+let fileName = "document.pdf";
+console.log(fileName.endsWith(".pdf"))
